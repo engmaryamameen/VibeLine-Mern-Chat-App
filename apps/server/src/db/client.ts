9c1fafa966ee540dd7e6de29aecbe08688643175
Maryam Ameen <engmaryamameen@gmail.com>
2026-02-20 19:04:18 +0500

    chore: add client.ts

---
+++ apps/server/src/db/client.ts
@@ -0,0 +1,15 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import pg from 'pg';
+
+import { env } from '@/config/env';
+import * as schema from './schema';
+
+const pool = new pg.Pool({
+  connectionString: env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000
+});
+
+export const db = drizzle(pool, { schema });
+export type Database = typeof db;
