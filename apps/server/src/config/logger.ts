0a0d0be1e8bebc84d73dde8b873e541a7418fc61
Maryam Ameen <engmaryamameen@gmail.com>
2026-02-20 19:04:18 +0500

    chore: update logger.ts

--- apps/server/src/config/logger.ts
+++ apps/server/src/config/logger.ts
@@ -1,3 +1,4 @@
+import pino from 'pino';
 import type { FastifyServerOptions } from 'fastify';
 
 import { env } from '@/config/env';
@@ -15,3 +16,18 @@ export const loggerConfig: FastifyServerOptions['logger'] =
         }
       }
     : true;
+
+export const logger = pino(
+  env.NODE_ENV === 'development'
+    ? {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+            colorize: true,
+            translateTime: 'SYS:standard'
+          }
+        }
+      }
+    : {}
+);
