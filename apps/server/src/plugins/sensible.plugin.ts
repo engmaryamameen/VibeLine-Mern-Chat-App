571f2f09b3ffa7df8da25a7b5095615bb453747d
Maryam Ameen <engmaryamameen@gmail.com>
2026-02-20 19:04:18 +0500

    chore: remove sensible.plugin.ts

--- apps/server/src/plugins/sensible.plugin.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyPluginAsync } from 'fastify';
-import fastifySensible from '@fastify/sensible';
-
-const sensiblePlugin: FastifyPluginAsync = async (app) => {
-  await app.register(fastifySensible);
-};
-
-export default sensiblePlugin;
